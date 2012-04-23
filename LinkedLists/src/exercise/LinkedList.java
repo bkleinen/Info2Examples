82279a2a58a7fd77b302b29b740ea29afc30dd82
B Kleinen <kleinen@infrastructure.de>
2012-04-23 22:02:15 +0200

    removed files renamed in eclipse

--- LinkedLists/src/exercise/LinkedList.java
+++
@@ -1,9 +0,0 @@
-package exercise;
-
-public interface LinkedList<E extends Comparable<E>> {
-	public void append(E s) ;
-	public boolean isEmpty() ;
-	public E pop();
-	public void insert(E data);
-	public E remove(E data);
-}
